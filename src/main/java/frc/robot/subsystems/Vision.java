35e70226ad08796547dd8f80f92e64aa7e70a7a9
Giulia-DS <giulia.dossantos@er9.org>
2024-01-16 20:04:30 -0500

    vision

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -7,8 +7,29 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
+
+  private final PhotonCamera camera;
+  private final PhotonPoseEstimator photonEstimator;
+  private double lastEstTimestamp = 0;
+
+  
+  private PhotonCameraSim cameraSim;
+  private VisionSystemSim visionSim;
+
+
   /** Creates a new Vision. */
-  public Vision() {}
+  public Vision() {
+    camera = new PhotonCamera("Arducam_OV9281_USB_Camera");
+    camera = new PhotonCamera("Global_Shutter_Camera");
+    
+    photonEstimator =
+                new PhotonPoseEstimator(
+                        kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, camera, kRobotToCam);
+        photonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+
+        Pose3d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(), aprilTagFieldLayout.getTagPose(target.getFiducialId()), cameraToRobot);
+
+  }
 
   @Override
   public void periodic() {
